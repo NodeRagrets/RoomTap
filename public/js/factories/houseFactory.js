e3da14c347eb82e5d9f38ddbf186d57ac80dffbf
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-05 15:15:28 -0800

    [refactor] started the http request

--- public/js/factories/houseFactory.js
+++ public/js/factories/houseFactory.js
@@ -1,4 +1,19 @@
 angular.module('houseFactory',[])
   .factory('House', ['$http', '$window', function($http, $window) {
 
+      var addRooms = function(rooms){
+
+          return $http({
+            method: 'POST',
+            url: '/api/', //TODO finish Url
+            data: rooms
+          }).then(function(response){
+            return response.data;
+          }).catch(function(error){
+            console.log(error);
+          })
+       }
+      return {
+        addRooms: addRooms
+      }
   }])
