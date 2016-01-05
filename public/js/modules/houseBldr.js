37a936a021df788cc86b0306b095a6bb44afca47
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-05 15:16:00 -0800

    public/js/modules/houseBldr.js

--- public/js/modules/houseBldr.js
+++ public/js/modules/houseBldr.js
@@ -1,4 +1,17 @@
 angular.module('houseBuilder', [])
-    .controller('houseBldr', '$scope', function($scope){
-      
+    .controller('houseBldr', '$scope', 'House', function($scope, House){
+        $scope.data = {};
+        $scope.data['roomName'] = '';
+        $scope.data['house'] = [];
+        $scope.addRoomToHouse = function(){
+          if($scope.data.roomName){
+            var name = $scope.data.roomName;
+            var array = $scope.data.house;
+            array.push(name);
+            //call function in factory TODO
+             House.addRooms($scope.data)
+          } else{
+            alert('You must enter a roomName');
+          }
+        }
     })
