e7562ed1daadf3326aa683b33647be64b3f95cb5
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-05 16:04:39 -0800

    [refactor] made changes to controller in houseBldrjs

--- public/js/modules/houseBldr.js
+++ public/js/modules/houseBldr.js
@@ -2,17 +2,20 @@ angular.module('houseBuilder', [])
     .controller('houseBldr', '$scope', 'House', function($scope, House){
         $scope.data = {};
         $scope.data['roomName'] = '';
+        $scope.data['roomType'] = '';
         $scope.data['house'] = [];
+
         $scope.addRoomToHouse = function(){
-          if($scope.data.roomName){
+          if($scope.data.roomName && $scope.data.roomType){
+            console.log($scope.data.roomName, "ROOMNAME");
+            console.log($scope.data.roomType, "ROOMTYPE");
             var name = $scope.data.roomName;
             var array = $scope.data.house;
             array.push(name);
-            //call function in factory TODO
              House.addRooms($scope.data)
           } else{
-            alert('You must enter a roomName');
+            alert('You must fill out input fields');
           }
         }
-        
+
     })
