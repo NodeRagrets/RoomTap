048df2591e4a1aa207dc6897858ba24270404575
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-06 11:41:47 -0800

    [refactor] added a test code for routing in houseController.js

--- server/house/houseController.js
+++ server/house/houseController.js
@@ -2,7 +2,10 @@ var express = require('express');
 var helpers = require('./../db/helpers.js');
 
 module.exports = {
-  build: function(){
-    
+  build: function(req, res){
+    if(!req.body){
+      res.sendStatus(404)
+    }
+    res.status(200).send(req.body)
   }
 }
