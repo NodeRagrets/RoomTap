a5eec854186182485ebc6c0e576367a30e5ca242
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-05 20:12:58 -0800

    [refactor] changed the name of the handler in eventRoutesjs

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -1,6 +1,6 @@
 var eventController = require('./eventController.js');
 
 module.exports = function(app) {
-  app.get('/events', eventController.getEvent);
+  app.get('/events', eventController.getEvents);
   app.post('/booked', eventController.postEvent);
 };
