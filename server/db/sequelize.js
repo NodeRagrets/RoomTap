426cd9b4f1af70f026ade11212ceb7eae94096e2
tbergen1 <tom@servant.co>
2016-01-06 11:34:14 -0800

    [feat] add database helper methods in db directory

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -58,43 +58,43 @@ models.RoomEventsJoin = sequelize.define('RoomEventsJoin', {});
 models.Homes.hasMany(models.Users);
 models.Homes.hasMany(models.Rooms);
 models.Homes.hasMany(models.Events);
-models.User.hasMany(models.Events);
+models.Users.hasMany(models.Events);
 
 models.Events.belongsToMany(models.Rooms, {through: 'RoomEventsJoin'});
 models.Rooms.belongsToMany(models.Events, {through: 'RoomEventsJoin'});
 
 models.Users.sync({force: false}).then(function() {
-  return models.Users.create({
-    username: 'ScroTom',
-    password: 'pass',
-    email: 'tom@gmail.com',
-    HomeId: 1
-  });
+  // return models.Users.create({
+  //   username: 'ScroTom',
+  //   password: 'pass',
+  //   email: 'tom@gmail.com',
+  //   HomeId: 1
+  // });
 });
 
 models.Homes.sync({force: false}).then(function() {
-    return models.Homes.create({
-    name: 'Da House',
-    address: '111 noderagrets Lane'
-  });
+  //   return models.Homes.create({
+  //   name: 'Da House',
+  //   address: '111 noderagrets Lane'
+  // });
 });
 
 models.Rooms.sync({force: false}).then(function() {
-    return models.Rooms.create({
-    name: 'The Dungeon',
-    type: 'Living Room',
-    HomeId: 1
-  });
+  //   return models.Rooms.create({
+  //   name: 'The Dungeon',
+  //   type: 'Living Room',
+  //   HomeId: 1
+  // });
 });
 
 models.Events.sync({force: false}).then(function() {
-    return models.Events.create({
-    name: 'Dungeon Party',
-    description: 'PARTY IN HERE',
-    date: 'Jan 6, 1999',
-    duration: 100,
-    HomeId: 1
-  });
+  //   return models.Events.create({
+  //   name: 'Dungeon Party',
+  //   description: 'PARTY IN HERE',
+  //   date: 'Jan 6, 1999',
+  //   duration: 100,
+  //   HomeId: 1
+  // });
 });
 
 models.RoomEventsJoin.sync({force: false}).then(function() {
