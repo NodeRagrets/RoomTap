6dce649c385f2e9085e24c05fd84d3706fc2dfe7
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-06 21:08:42 -0800

    [feat] completed utility functions for JWTs, utility.js

--- server/utility/utility.js
+++ server/utility/utility.js
@@ -1,17 +1,41 @@
 var jwt = require('jwt-simple');
 
+var secret = process.env.TOKEN_SECRET || 'WILDCARDBOOYAHBOOYAHBOOYAH';
+
 module.exports = {
-  decode: function(req, res, next) {
-    var token = req.headers["x-access-token"];
-    if(!token) {
-      return res.status(404).end();
-    }
-    try { var user = jwt.decode(token, 'WILDCARD');
-      req.user = user;
+
+  issueToken: function(payload){
+    var token = jwt.encode(payload, secret);
+    return token;
+  },
+
+  verifyToken: function(token) {
+    var decoded = jwt.decode(token, secret);
+    return decoded;
+  },
+
+  hasToken: function(req, res, next) {
+    var token = req.headers['authorization'];
+    var validToken;
+
+    if(req.headers && token) {
+      validToken = module.exports.verifyToken(token);
+      console.log(!validToken);
+      if(!validToken) {
+        res.status(401).send();
+      }
+      req.token = validToken;
       next();
-    } 
-    catch(err) { 
-      return next(err);
+    } else {
+      res.status(401).send();
     }
+  },
+
+  extendPayload: function(payload, newData) {
+    for(var data in newData) {
+      payload[data] = newData[data];
+    } 
+    return payload;
   }
+
 };
