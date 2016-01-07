cbeef242ee867470f65de0b6b266d1e4ed0c238c
Rachel Jenkins <jenkinsr07@gmail.com>
2016-01-06 21:41:45 -0800

    [update] updated routing per new utility functions, routeconfig.js

--- server/config/routeconfig.js
+++ server/config/routeconfig.js
@@ -1,24 +1,25 @@
 var bodyParser = require('body-parser');
 var helpers = require('./helpers.js');
-var utility = require(__dirname + '/../utility/utility.js');
+var utils = require('./../utility/utility.js');
 
 module.exports = function(app, express) {
   app.use(bodyParser.json());
   //creating routes for each individual moduels (groups of routes)
-  var houseRouter = express.Router();
-  require(__dirname + './../house/houseRoutes.js')(houseRouter);
-  app.use('/api/houses', houseRouter);
 
   var userRouter = express.Router();
-  require(__dirname + '/../users/userRoutes.js')(userRouter);
-  // app.use('/api/users', utility.decode);
-  // This won't work at this point because there is no login;
-  app.use('/api/users', userRouter);
   var eventRouter = express.Router();
+  var houseRouter = express.Router();
+
+  // This won't work at this point because there is no login;
+
+  app.use('/api/users', userRouter);
+  // app.use('/api/events', utils.hasToken);
+  app.use('/api/events', utils.hasToken, eventRouter);
+  app.use('/api/houses', utils.hasToken, houseRouter);
+
+
+  require(__dirname + './../house/houseRoutes.js')(houseRouter);
+  require(__dirname + '/../users/userRoutes.js')(userRouter);
   require(__dirname + '/../events/eventRoutes.js')(eventRouter);
-  app.use('/api/events', utility.decode);
-  app.use('/api/events', eventRouter);
-
-
 
 };
