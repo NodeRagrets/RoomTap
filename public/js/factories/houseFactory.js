1fd66108b5e3d7d42d5d60ca607fdc56abe706be
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-07 12:14:49 -0800

    [refactor] added addHouse function to houseFactory.js

--- public/js/factories/houseFactory.js
+++ public/js/factories/houseFactory.js
@@ -1,19 +1,31 @@
 angular.module('houseFactory',[])
   .factory('House', ['$http', '$window', function($http, $window) {
+      var addHouse = function(house){
 
+        return $http({
+          method: 'POST',
+          url: '/api/houses/address',
+          data: house
+        }).then(function(response){
+          return response.data;
+        }).catch(function(error){
+          console.log('ERROR IN ADDHOUSE FACTORY')
+        })
+      }
       var addRooms = function(rooms){
 
           return $http({
             method: 'POST',
-            url: '/api/houses/build', //TODO finish Url
+            url: '/api/houses/build',
             data: rooms
           }).then(function(response){
             return response.data;
           }).catch(function(error){
-            console.log(error);
+            console.log('ERROR IN ADDROOMS FACTORY', error);
           })
        }
       return {
+        addHouse: addHouse,
         addRooms: addRooms
       }
   }])
