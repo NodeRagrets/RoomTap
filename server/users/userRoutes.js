ca6ba8e58c0d4f7ab5487b5f1c465b8761bde6a6
malekascha <malekascha@gmail.com>
2016-01-07 19:24:37 -0800

    [refactor] set up account creation with FB in directory users

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,6 +2,8 @@ var userController = require('./userController.js');
 
 module.exports = function(app) {
   app.post('/facebookAuth',  userController.facebookAuth);
-  app.post('/signup', userController.signup);
+  app.post('/signup', userController.signup, userController.FBsignup);
   app.post('/login', userController.login);
+  // app.get('/facebookAuth',  function(){console.log('geh')});
+
 };
