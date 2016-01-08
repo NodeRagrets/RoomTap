7269a71d3c1923c664d1d1ee7dc0697c40c13748
tbergen1 <tom@servant.co>
2016-01-07 18:32:06 -0800

    [feat] add sockets to handle real time updating on server in server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var morgan = require('morgan');
 
-
 // initialize express
 
+var homes = [];
 
 //checking to see if PORT# is defined otherwise use 3000
 
@@ -23,3 +23,16 @@ app.use(express.static(__dirname +  "/../public"));
 app.use(morgan('dev'));
 
 server.listen(port);
+
+// var eventPage = io.of('/events'); 
+io.sockets.on('connection', function(socket) {
+  console.log('connect to events');
+  socket.emit('message', {'message': 'hello world'});
+  socket.on('load house', function(house) {
+    socket.join(house.address);
+  });
+
+  socket.on('event update', function(data) {
+    io.sockets.to(data.house).emit('received', data);
+  });
+});
