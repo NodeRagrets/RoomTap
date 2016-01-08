b2d45549b99a3cb135648101626954865e5f6e65
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-01-07 16:50:02 -0800

    [refactor] added users route in houseRoutes.js

--- server/house/houseRoutes.js
+++ server/house/houseRoutes.js
@@ -1,6 +1,7 @@
 var houseController = require('./houseController.js');
 
 module.exports = function(app) {
-  app.post('/address', houseController.address)
+  app.post('/address', houseController.address);
   app.post('/build', houseController.build);
+  app.post('/users', houseController.users);
 };
